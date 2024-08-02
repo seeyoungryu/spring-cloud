b9a0ac37363ec9239e32bbc0ec1063fcfbd11193
seeyoungryu <seayeux@gmail.com>
2024-08-03 01:57:41 +0900

    Initial commit with server, product, and order projects

---
+++ product/src/main/java/com/spring_cloud/eureka/client/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.spring_cloud.eureka.client.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
